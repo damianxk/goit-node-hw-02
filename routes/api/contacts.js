87677f40fac151b7265613d0cbf1acad7b937acf
Damian Kowalczyk <damiank209@gmail.com>
2023-11-15 21:17:00 +0100

    app update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,130 @@
-const express = require('express')
+import express from "express";
 
-const router = express.Router()
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updatedStatusContact,
+} from "./../../models/contacts.js";
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+export const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    return res.json({
+      status: "success",
+      code: 200,
+      data: { contacts },
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json(`An error occurred while getting the contact list: ${err}`);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  console.log("this is id: ", id);
+  try {
+    const contact = await getContactById(id);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    return res.json({
+      status: "success",
+      code: 200,
+      data: { contact },
+    });
+  } catch (err) {
+    res.status(500).json(`An error occurred while getting the contact: ${err}`);
+  }
+});
 
-module.exports = router
+router.post("/", async (req, res, next) => {
+  const { body } = req;
+
+  if (Object.keys(body).length === 0) {
+    return res
+      .status(400)
+      .json("Error! Missing fields! Empty request is not allowed");
+  }
+
+  try {
+    const contact = await addContact(body);
+
+    return res.status(201).json({
+      status: "success",
+      code: 201,
+      data: { contact },
+    });
+  } catch (err) {
+    res.status(500).json(`An error occurred while adding the contact: ${err}`);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const isContactRemoved = await removeContact(id);
+
+    return res.status(200).json({
+      message: `Contact with ID ${id} has been successfully removed.`,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json(`An error occurred while removing the contact: ${err}`);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  if (Object.keys(body).length === 0) {
+    return res
+      .status(400)
+      .json("Error! Missing fields! Empty request is not allowed");
+  }
+
+  try {
+    const updatedContact = await updateContact(id, body);
+
+    return res.json({
+      status: "success",
+      code: 200,
+      data: { updatedContact },
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json(`An error occurred while updating the contact: ${err}`);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+  const { favorite } = body;
+
+  if (!("favorite" in body) || Object.keys(body).length === 0) {
+    return res.status(400).json("Error! Missing field favorite!");
+  }
+
+  try {
+    const updatedStatus = await updatedStatusContact(id, favorite);
+
+    return res.json({
+      status: "success",
+      code: 200,
+      data: { updatedStatus },
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json(`An error occurred while updating the contact: ${err}`);
+  }
+});
