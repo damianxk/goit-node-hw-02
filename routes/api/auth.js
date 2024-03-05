0202a667ef81cbefc4e73b56f09c4642059926b9
Damian Kowalczyk <damiank209@gmail.com>
2024-03-05 21:35:06 +0100

    app done

---
+++ routes/api/auth.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import { ctrlTask } from '../../assets/ctrlTask.js';
+import { validateBody } from '../../middleware/validateBody.js';
+import { authenticate } from '../../middleware/authenticate.js';
+import { upload } from '../../middleware/upload.js';
+import {
+  register,
+  login,
+  logout,
+  getCurrent,
+  updateUserSubscription,
+  updateAvatar,
+  verify,
+  resendEmail,
+} from '../../controllers/authenticationJwt.js';
+import { schemas } from '../../models/user.js';
+
+export const authRouter = express.Router();
+
+//rejestracja
+authRouter.post('/signup', validateBody(schemas.registerSchema), ctrlTask(register));
+authRouter.get('/verify/:verificationToken', ctrlTask(verify));
+authRouter.post('/verify', validateBody(schemas.verifyEmailSchema), ctrlTask(resendEmail));
+
+//logowanie
+authRouter.post('/login', validateBody(schemas.loginSchema), ctrlTask(login));
+authRouter.get('/current', authenticate, ctrlTask(getCurrent));
+authRouter.get('/logout', authenticate, ctrlTask(logout));
+authRouter.patch(
+  '/updatesubscription',
+  authenticate,
+  validateBody(schemas.updateSubscriptionSchema),
+  ctrlTask(updateUserSubscription)
+);
+authRouter.patch('/avatars', authenticate, upload.single('avatar'), ctrlTask(updateAvatar));
--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const app = require('./app')
+import { app } from './app.js';
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+config();
+
+const port = 3000;
+const server = 'http://localhost';
+export const serverAddress = `${server}:${port}`;
+
+const uriDb = process.env.DB_HOST;
+
+const runServer = async () => {
+  try {
+    const connection = await mongoose.connect(uriDb);
+    console.log('Database connection successful');
+    app.listen(port, () => {
+      console.log('Server running. Use our API on port: 3000');
+      console.log('>>> Press Ctrl+C to stop <<<');
+    });
+  } catch (error) {
+    console.log('Cannot connect to MongoDB');
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runServer();
